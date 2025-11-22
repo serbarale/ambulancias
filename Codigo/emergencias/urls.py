dad5d4a6a43e46c373dc3dda1325f90dcf70f345
AsadoREDACTADO <sebarrantes@outlook.com>
2025-11-22 18:51:11 -0500

    URL Tests for Django

--- Codigo/emergencias/urls.py
+++ Codigo/emergencias/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('informes/registrar/<int:ambulancia_id>/',views.registrar_informe, name='registrar_informe'),
     path("informes/pacientes/", views.listar_informes_desde_pacientes, name="lista_informes_pacientes"),
     path('informes/asignar/', views.asignar_ambulancia, name='asignar_ambulancia'),
-    path("informes/registrar/", views.registrar_informe, name="registrar_informe"),
     path("informes/<int:informe_id>/detalles/", views.detalles_informe, name="detalles_informe"),
     path("informes/<int:id>/revocacion/", views.generar_revocacion, name="generar_revocacion"),
     path("informes/<int:id>/consentimiento/", views.generar_consentimiento, name="generar_consentimiento"),
